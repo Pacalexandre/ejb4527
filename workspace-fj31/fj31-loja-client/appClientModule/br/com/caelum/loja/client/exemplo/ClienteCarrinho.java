992d08d41597b10ca9384346c00c0e21817b39c7
GECOS <ejb4527@caelum-20121024-1.(none)>
2014-09-10 12:13:50 -0300

    staless

--- workspace-fj31/fj31-loja-client/appClientModule/br/com/caelum/loja/client/exemplo/ClienteCarrinho.java
+++ workspace-fj31/fj31-loja-client/appClientModule/br/com/caelum/loja/client/exemplo/ClienteCarrinho.java
@@ -9,6 +9,25 @@ import br.com.caelum.loja.session.Carrinho;
 public class ClienteCarrinho {
 	public static void main(String[] args) throws NamingException {
 		InitialContext ic = new InitialContext();
+		
+		for (int i = 0; i < 10; i++) {
+			Carrinho carrinho = (Carrinho)ic.lookup("ejb:fj31-loja-ear/fj31-loja-ejb3/CarrinhoBean!br.com.caelum.loja.session.Carrinho?stateful");
+			
+			Livro l1 = new Livro();
+			l1.setNome("Fausto");
+			l1.setPreco(150);
+			
+			carrinho.addLivro(l1);
+			
+			Livro l2 = new Livro();
+			l2.setNome("Pais e Filhos");
+			l2.setPreco(55);
+			
+			carrinho.addLivro(l2);
+			
+			System.out.println("Total: " + carrinho.getTotal());
+			
+		}
 		Carrinho carrinho = (Carrinho)ic.lookup("ejb:fj31-loja-ear/fj31-loja-ejb3/CarrinhoBean!br.com.caelum.loja.session.Carrinho?stateful");
 		
 		Livro l1 = new Livro();
@@ -25,6 +44,8 @@ public class ClienteCarrinho {
 		
 		System.out.println("Total: " + carrinho.getTotal());
 		
-		carrinho.finalizaCompra();
+		//carrinho.finalizaCompra();
+		
+		//carrinho.addLivro(l2);
 	}
 }
--- workspace-fj31/fj31-loja-ejb3/ejbModule/br/com/caelum/loja/session/CarrinhoBean.java
+++ workspace-fj31/fj31-loja-ejb3/ejbModule/br/com/caelum/loja/session/CarrinhoBean.java
@@ -2,14 +2,24 @@ package br.com.caelum.loja.session;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
+import javax.annotation.PreDestroy;
+import javax.ejb.PostActivate;
+import javax.ejb.PrePassivate;
 import javax.ejb.Remote;
+import javax.ejb.Remove;
 import javax.ejb.Stateful;
+import javax.ejb.StatefulTimeout;
+
+import org.jboss.ejb3.annotation.Cache;
 
 import br.com.caelum.loja.entity.Livro;
 
 @Stateful
 @Remote(Carrinho.class)
+@Cache("passivating")
+@StatefulTimeout(value = 30,unit= TimeUnit.SECONDS)
 public class CarrinhoBean implements Carrinho {
 
 	private double total;
@@ -33,11 +43,25 @@ public class CarrinhoBean implements Carrinho {
 		return this.total;
 	}
 
-	@Override
+	@Remove
 	public void finalizaCompra() {
 		for (Livro livro : this.livros) {
 			System.out.println("Comprando livro: " + livro.getNome());
 		}
 	}
 	
+	@PreDestroy
+	public void destroy(){
+		System.out.println("Removendo carrinho da memoria");
+	}
+	
+	@PostActivate
+	public void ativando(){
+		System.out.println("Ativando " + this);
+	}
+	
+	@PrePassivate
+	private void passivando(){
+		System.out.println("Passivando "+ this);
+	}
 }
