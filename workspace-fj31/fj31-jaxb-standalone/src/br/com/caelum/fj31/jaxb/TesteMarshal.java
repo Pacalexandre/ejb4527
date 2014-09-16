8cd47f76f143ea90d42fa8d9e77e59558f5be0a7
GECOS <ejb4527@caelum-20121024-1.(none)>
2014-09-16 12:09:35 -0300

    jms

---
+++ workspace-fj31/fj31-jaxb-standalone/src/br/com/caelum/fj31/jaxb/TesteMarshal.java
@@ -0,0 +1,27 @@
+package br.com.caelum.fj31.jaxb;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+
+public class TesteMarshal {
+
+	public static void main(String[] args) throws JAXBException, FileNotFoundException {
+		Produto produto = new Produto();
+		Categoria categoria = new Categoria();
+		categoria.setNome("esporte");
+		produto.setNome("bola");
+		produto.setPreco(20.34);
+		produto.setDescricao("bola quadrada");
+		produto.setCategoria(categoria);
+		
+		JAXBContext context = JAXBContext.newInstance(Produto.class);
+		Marshaller marshaller = context.createMarshaller();
+		marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT,true);
+		marshaller.marshal(produto, new FileOutputStream("bola.xml"));
+	}
+
+}
